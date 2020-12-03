4990d1446d245a2d0e8e456a8537d610b1ba9925
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2020-12-03 11:33:46 +0000

    Implemented SignUp service.

--- flutter_project/lib/services/AuthenticationService.dart
+++ flutter_project/lib/services/AuthenticationService.dart
@@ -19,15 +19,37 @@ class AuthenticationService extends Service {
       })
     );
 
-
     if (response.statusCode != 200)
       throw new Exception("Login not authorized.");
 
     return User.fromJson(json.decode(response.body));
   }
 
-  Future<User> signUp(User user) async {
+  Future<User> signUp(Map<String, String> formValues) async {
 
+    Map<String, dynamic> json = {
+      'username': formValues['username'],
+      'email': formValues['email'],
+      'password': formValues['password'],
+      'type': formValues['type'],
+      'name': formValues['name'],
+      'imagePath': formValues['imagePath']
+    };
+
+    if (formValues['type'] == "ARTIST")
+      json['description'] = formValues['description'];
+
+    final http.Response response = await http.post(
+      this.apiURL + "/user/signup",
+      headers: headersPost,
+      body: jsonEncode(json)
+    );
+
+    if(response.statusCode != 200)
+      throw new Exception("Signup not successful.");
+
+    json.remove('password');
+    return new User.fromJson(json);
   }
 
 }

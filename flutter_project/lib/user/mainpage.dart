14091e9e620a15ea2fc9fbc54f5f11ba3bacae3f
anocas-22 <anasofiamsilva22@gmail.com>
2020-12-04 19:13:27 +0000

    dynamic main page (not working)

--- flutter_project/lib/user/mainpage.dart
+++ flutter_project/lib/user/mainpage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../models/concert.dart';
 import 'package:flutter_complete_guide/models/user.dart';
 import 'package:flutter_complete_guide/utils/widgets.dart';
+import 'package:flutter_complete_guide/services/ConcertService.dart';
 
 class UserMainPage extends StatefulWidget {
   UserMainPage({Key key}) : super(key: key);
@@ -12,12 +14,63 @@ class UserMainPage extends StatefulWidget {
 
 class UserMainPageState extends State<UserMainPage> {
 
+  ConcertService concertService = new ConcertService();
+
+  Widget fanMainPage() {
+    return FutureBuilder(
+      future: getConcerts(),
+      builder: (context, concerts) {
+        if (!concerts.hasData) {
+          print('concerts data ${concerts.data}');
+          return Column(
+            children: [
+              Text("the data is empty"),
+              Center(child: CircularProgressIndicator())
+            ]
+          );
+        }
+        return ListView.builder(
+          itemCount: concerts.data.length,
+          itemBuilder: (context, index) {
+            Concert concert = concerts.data[index];
+            return Column(
+              children: <Widget>[
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  child: new InkWell(
+                    onTap: () {  //TODO: ecrã com info dinâmica
+                      Navigator.pushNamed(context, "/user/concertInfo");
+                    },
+                    child: Column(
+                      children: [
+                        Image.asset(concert.image),
+                        ListTile(
+                          leading: Image.asset('assets/images/mini_james.png'),
+                          title: Text(concert.name),
+                          subtitle: Text(
+                            'Artist: ' + concert.username,
+                            style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
   Widget createFanMenu(BuildContext context) {
     return MainMenu(
-      context,
-        ListView(
+      context, fanMainPage()
+        /*ListView(
           children: [
-            Card(
+            /*Card(
               clipBehavior: Clip.antiAlias,
               elevation: 5,
               child: new InkWell(
@@ -123,14 +176,23 @@ class UserMainPageState extends State<UserMainPage> {
                   ),
                 ],
               ),
-            )
+            )*/
           ],
-        )
+        )*/
     );
   }
 
   Widget createArtistMenu(BuildContext context) {
     //TODO: Put the artist pages here!
+  }
+
+  Future<List<Concert>> getConcerts() async {
+    try {
+      List<Concert> concerts = await concertService.getAllConcerts();
+      return concerts;
+    } catch(e) {
+      print(e.toString());
+    }
   }
 
   @override
